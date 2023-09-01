4264d5b0aa65fd62d8bbbc9cb02038d476bf7f27
Shariar Islam <shariarn85@gmail.com>
2023-09-01 18:13:27 +0600

    empty category added

--- index.js
+++ index.js
@@ -20,49 +20,62 @@ const loadCategoryPost = async (categoryId) => {
     const data = await response.json();
     const allPost = data.data;
 
+    console.log(allPost);
     const postContainer = document.getElementById('post-container')
     postContainer.textContent = ''
-
-    allPost?.forEach((post) => {
-
-        /*  let timeInSeconds = post?.others?.posted_date;
-         let timeInMinutes = Math.floor(timeInSeconds / 60)
-         let timeInHours = Math.floor(timeInMinutes / 60) */
-
-        let hours = Math.floor(post?.others?.posted_date / 3600);
-        let minutes = Math.floor((post?.others?.posted_date - (hours * 3600)) / 60);
-        let time = hours + 'hrs ' + minutes + 'min ago';
-
-        const viewsString = post?.others?.views;
-        const viewsSplit = viewsString.split("k")
-        console.log(viewsSplit);
-
-
-        const div = document.createElement('div')
-        div.innerHTML = `
-            <div class="card text-center md:text-left">
-                <div class="relative">
-                <figure><img class="h-[250px] " src="${post?.thumbnail}" alt="Shoes" /></figure>
-                <p class="text-white bg-gray-500  absolute top-48 right-10 p-2">${post?.others?.posted_date ? time : ''}</p>
-                </div>
-                
-            <div class="card-body">
-                
-                <h2 class="card-title justify-center md:justify-start"> <img class="w-10 h-10 rounded-full" src="${post?.authors[0]?.profile_picture}"> ${post?.title}</h2>
-                <p>
-                ${post?.authors[0]?.profile_name} ${post?.authors[0]?.verified ? '<img class="w-5 inline" src="./images/verified.png">' : ''}
-                </p>
-                <p>${post?.others?.views} views</p>
+    
+    if (allPost.length === 0) {
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <div class="card text-center md:text-center">
+                <div class="container mx-auto">
+                    <img class="text-center" src="./images/Icon.png" alt="Shoes" />
+                    <p class="font-bold text-center text-2xl">No data to show in this category</p>
+                </div>    
             </div>
-          </div>
-            `
-
+                `
         postContainer.appendChild(div)
-    })
+        console.log('no data');
+    }
+    else {
+        allPost?.forEach((post) => {
 
+            /*  let timeInSeconds = post?.others?.posted_date;
+             let timeInMinutes = Math.floor(timeInSeconds / 60)
+             let timeInHours = Math.floor(timeInMinutes / 60) */
+    
+            let hours = Math.floor(post?.others?.posted_date / 3600);
+            let minutes = Math.floor((post?.others?.posted_date - (hours * 3600)) / 60);
+            let time = hours + 'hrs ' + minutes + 'min ago';
+    
+            const viewsString = post?.others?.views;
+            const viewsSplit = viewsString.split("k")
+            // console.log(viewsSplit);
+
+            const div = document.createElement('div')
+            div.innerHTML = `
+                <div class="card text-center md:text-left">
+                    <div class="relative">
+                    <figure><img class="h-[250px] " src="${post?.thumbnail}" alt="Shoes" /></figure>
+                    <p class="text-white bg-black  absolute top-48 right-10">${post?.others?.posted_date ? time : ''}</p>
+                    </div>
+                    
+                <div class="card-body">
+                    
+                    <h2 class="card-title justify-center md:justify-start"> <img class="w-10 h-10 rounded-full" src="${post?.authors[0]?.profile_picture}"> ${post?.title}</h2>
+                    <p>
+                    ${post?.authors[0]?.profile_name} ${post?.authors[0]?.verified ? '<img class="w-5 inline" src="./images/verified.png">' : ''}
+                    </p>
+                    <p>${post?.others?.views} views</p>
+                </div>
+              </div>
+                `
+            postContainer.appendChild(div)
+        })
+    }
 }
 
 
 loadCategory()
 
-loadCategoryPost('1000')
+loadCategoryPost('1005')
